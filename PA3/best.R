8ebee5b734d75abc6379c3dea8064cd99a1271b7
Carlos Barillas <carlos.barillas@inrix.com>
2019-08-17 20:36:07 -0700

    added PA3 directory, files for PA3 assignment and completed best function in best.R

---
+++ PA3/best.R
@@ -0,0 +1,38 @@
+best <- function(state, outcome) {
+        ## Read outcome data
+        outcome_data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+
+        ## Check that state and outcome are valid
+        if (!(is.element(state, outcome_data[,7]))) {
+                stop("invalid state")
+        }
+        if (!(is.element(outcome, c("heart attack", "heart failure", "pneumonia")))) {
+                stop("invalid outcome")
+        }
+        
+        
+        
+        ## Return hospital name in that state with lowest 30-day death
+        ## rate
+        
+        sub_outcome <- subset(outcome_data, State == state)
+        
+        if (outcome == "heart attack") {
+                sub_outcome[,11] <- as.numeric(sub_outcome[,11])
+                min_value <- min(sub_outcome[,11], na.rm=T)
+                min_row <- subset(sub_outcome, sub_outcome[,11] == min_value)
+        }
+        else if (outcome == "heart failure"){
+                sub_outcome[,17] <- as.numeric(sub_outcome[,17])
+                min_value <- min(sub_outcome[,17], na.rm=T)
+                min_row <- subset(sub_outcome, sub_outcome[,17] == min_value)
+        }
+        else if (outcome == "pneumonia"){
+                sub_outcome[,23] <- as.numeric(sub_outcome[,23])
+                min_value <- min(sub_outcome[,23], na.rm=T)
+                min_row <- subset(sub_outcome, sub_outcome[,23] == min_value)
+        }
+        print(min_row$Hospital.Name)
+        print(class(min_row$Hospital.Name))
+
+}
